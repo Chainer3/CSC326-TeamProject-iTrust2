26bbb4dbbdbba419cebfc8105318c65d0e209fe4
lrpears2 <lrpears2@ncsu.edu>
2022-03-31 23:46:55 -0400

    Added billing role and controller class

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/api/APIUserController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/api/APIUserController.java
@@ -67,9 +67,12 @@ public class APIUserController extends APIController {
     /** Constant for vaccinator role */
     private static final String       ROLE_VACCINATOR = "ROLE_VACCINATOR";
 
+    /** Constant for billing role */
+    private static final String       ROLE_BILLING    = "ROLE_BILLING";
+
     /** All roles */
     private static final List<String> ALL_ROLES       = List.of( ROLE_ADMIN, ROLE_PATIENT, ROLE_HCP, ROLE_ER,
-            ROLE_LABTECH, ROLE_VIROLOGIST, ROLE_OD, ROLE_OPH, ROLE_VACCINATOR );
+            ROLE_LABTECH, ROLE_VIROLOGIST, ROLE_OD, ROLE_OPH, ROLE_VACCINATOR, ROLE_BILLING );
 
     /** LoggerUtil */
     @Autowired
@@ -262,6 +265,10 @@ public class APIUserController extends APIController {
         userService.save( patient );
 
         userService.save( vaccinator );
+
+        final User billing = new Personnel( new UserForm( "billing", "123456", Role.ROLE_BILLING, 1 ) );
+
+        userService.save( billing );
 
         loggerUtil.log( TransactionType.USERS_GENERATED, "" );
 
---
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/routing/BillingController.java
@@ -0,0 +1,59 @@
+package edu.ncsu.csc.iTrust2.controllers.routing;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.ncsu.csc.iTrust2.models.enums.Role;
+
+/**
+ * Controller class responsible for managing the behavior for the Billing
+ * Landing Screen
+ *
+ * @author Lydia Pearson
+ *
+ */
+@Controller
+public class BillingController {
+    /**
+     * Returns the Landing screen for the Billing Staff
+     *
+     * @param model
+     *            Data from the front end
+     * @return The page to display
+     */
+    @RequestMapping ( value = "billing/index" )
+    @PreAuthorize ( "hasAnyRole('ROLE_BILLING')" )
+    public String index ( final Model model ) {
+        return Role.ROLE_BILLING.getLanding();
+    }
+
+    /**
+     * Returns the page for managing CPT codes
+     *
+     * @param model
+     *            Data for the front end
+     * @return The page to display to the user
+     */
+    @GetMapping ( "/billing/manageCPTCodes" )
+    @PreAuthorize ( "hasAnyRole('ROLE_BILLING')" )
+    public String manageCPTCodes ( final Model model ) {
+        return "billing/manageCPTCodes";
+    }
+
+    /**
+     * Returns the page for the archived CPT code list
+     *
+     * @param model
+     *            The data for the front end
+     * @return Page to display to the user
+     */
+    @GetMapping ( "/billing/archivedCPTCodes" )
+    @PreAuthorize ( "hasAnyRole('ROLE_BILLING')" )
+    public String archivedCPTCodes ( final Model model ) {
+        return "/billing/archivedCPTCodes";
+    }
+
+}
