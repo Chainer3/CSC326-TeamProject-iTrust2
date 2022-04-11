00b8f66a8c9ef5718452bf8d657bf91c44967e4b
Alex Bernard <ajberna2@ncsu.edu>
2022-04-10 21:01:16 -0400

    Created outline for payBills.html page

--- iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/routing/BillingController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/iTrust2/controllers/routing/BillingController.java
@@ -56,4 +56,15 @@ public class BillingController {
         return "/billing/archivedCPTCodes";
     }
 
+    /*
+     * Returns the page for the Pay Bills page
+     * @param model The data for the front end
+     * @return The page to display to the user
+     */
+    @GetMapping ( "/billing/payBills" )
+    @PreAuthorize ( "hasAnyRole('ROLE_BILLING')" )
+    public String payBills ( final Model model ) {
+        return "billing/payBills";
+    }
+
 }
