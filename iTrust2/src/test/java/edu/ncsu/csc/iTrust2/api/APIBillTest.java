b8b1484cafa8d5145feb2398a0c6022b73956445
Zachary Harris <zmharri2@ncsu.edu>
2022-04-13 19:35:05 -0400

    Added additional set up and testing to API bill tests.

--- iTrust2/src/test/java/edu/ncsu/csc/iTrust2/api/APIBillTest.java
+++ iTrust2/src/test/java/edu/ncsu/csc/iTrust2/api/APIBillTest.java
@@ -1,5 +1,11 @@
 package edu.ncsu.csc.iTrust2.api;
 
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.junit.Before;
@@ -14,10 +20,22 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.google.gson.reflect.TypeToken;
+
+import edu.ncsu.csc.iTrust2.common.TestUtils;
+import edu.ncsu.csc.iTrust2.forms.OfficeVisitForm;
 import edu.ncsu.csc.iTrust2.forms.UserForm;
+import edu.ncsu.csc.iTrust2.models.Bill;
+import edu.ncsu.csc.iTrust2.models.Hospital;
+import edu.ncsu.csc.iTrust2.models.Patient;
 import edu.ncsu.csc.iTrust2.models.Personnel;
 import edu.ncsu.csc.iTrust2.models.User;
+import edu.ncsu.csc.iTrust2.models.enums.AppointmentType;
 import edu.ncsu.csc.iTrust2.models.enums.Role;
+import edu.ncsu.csc.iTrust2.models.enums.State;
+import edu.ncsu.csc.iTrust2.services.BillService;
+import edu.ncsu.csc.iTrust2.services.HospitalService;
+import edu.ncsu.csc.iTrust2.services.OfficeVisitService;
 import edu.ncsu.csc.iTrust2.services.UserService;
 
 /**
@@ -35,6 +53,15 @@ public class APIBillTest {
     private WebApplicationContext context;
 
     @Autowired
+    private OfficeVisitService    officeVisitService;
+
+    @Autowired
+    private BillService           billService;
+
+    @Autowired
+    private HospitalService       hospitalService;
+
+    @Autowired
     private UserService<User>     uService;
 
     /**
@@ -45,14 +72,46 @@ public class APIBillTest {
     @Before
     public void setup () throws Exception {
         mvc = MockMvcBuilders.webAppContextSetup( context ).build();
+        final UserForm pTestUser = new UserForm( "hcp", "123456", Role.ROLE_HCP, 1 );
         final UserForm pTestUser1 = new UserForm( "billing", "123456", Role.ROLE_BILLING, 1 );
         final UserForm pTestUser2 = new UserForm( "patient", "123456", Role.ROLE_PATIENT, 1 );
 
-        final Personnel p = new Personnel( pTestUser1 );
-        final Personnel p2 = new Personnel( pTestUser2 );
+        final Personnel p = new Personnel( pTestUser );
+        final Personnel p1 = new Personnel( pTestUser1 );
+        final Patient p2 = new Patient( pTestUser2 );
 
         uService.save( p );
+        uService.save( p1 );
         uService.save( p2 );
+
+        officeVisitService.deleteAll();
+        billService.deleteAll();
+
+        final Hospital hosp = new Hospital();
+        hosp.setAddress( "123 Raleigh Road" );
+        hosp.setState( State.NC );
+        hosp.setZip( "27514" );
+        hosp.setName( "iTrust Test Hospital 2" );
+
+        hospitalService.save( hosp );
+    }
+
+    /**
+     * Creates an office visit for testing.
+     *
+     * @throws Exception
+     */
+    private void setupOfficeVisit () throws Exception {
+        final OfficeVisitForm visit = new OfficeVisitForm();
+        visit.setDate( "2030-11-19T04:50:00.000-05:00" );
+        visit.setHcp( "hcp" );
+        visit.setPatient( "patient" );
+        visit.setNotes( "Test office visit" );
+        visit.setType( AppointmentType.GENERAL_CHECKUP.toString() );
+        visit.setHospital( "iTrust Test Hospital 2" );
+
+        /* Create the Office Visit */
+        officeVisitService.save( officeVisitService.build( visit ) );
     }
 
     /**
@@ -64,7 +123,14 @@ public class APIBillTest {
     @WithMockUser ( username = "billing", roles = { "USER", "BILLING", "ADMIN" } )
     @Transactional
     public void testBillingAPI () throws Exception {
+        setupOfficeVisit();
 
+        // Verify bills have been added
+        final String allBillsContent = mvc.perform( get( "/api/v1/bills/" ) ).andExpect( status().isOk() ).andReturn()
+                .getResponse().getContentAsString();
+        final List<Bill> allBills = TestUtils.gson().fromJson( allBillsContent, new TypeToken<List<Bill>>() {
+        }.getType() );
+        assertEquals( 1, allBills.size() );
     }
 
     /**
@@ -76,7 +142,14 @@ public class APIBillTest {
     @WithMockUser ( username = "patient", roles = { "USER", "PATIENT" } )
     @Transactional
     public void testPatientBillingAPI () throws Exception {
+        setupOfficeVisit();
 
+        // Verify bills have been added
+        final String allBillsContent = mvc.perform( get( "/api/v1/patient/bills/" ) ).andExpect( status().isOk() )
+                .andReturn().getResponse().getContentAsString();
+        final List<Bill> allBills = TestUtils.gson().fromJson( allBillsContent, new TypeToken<List<Bill>>() {
+        }.getType() );
+        assertEquals( 1, allBills.size() );
     }
 
 }
